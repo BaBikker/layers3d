d3a8635b9aef30fd56b5f03527991f5077bb5dd6
BaBikker <104166110+BaBikker@users.noreply.github.com>
2023-07-25 15:42:36 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/public/index.html")
+})
+
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname + "/public/contact.html")
+})
+
+app.get('/3dprintservice', (req, res) => {
+  res.sendFile(__dirname + "/public/3dprintservice.html")
+})
+
+app.get('/offerte', (req, res) => {
+  res.sendFile(__dirname + "/public/offerte.html")
+})
+
+app.get('/prijzen', (req, res) => {
+  res.sendFile(__dirname + "/public/prijzen.html")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
