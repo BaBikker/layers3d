5c242ca70c5cb7fce3ae9a9e2cf23f80d5013ebe
BaBikker <104166110+BaBikker@users.noreply.github.com>
2023-08-07 15:10:11 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express')
 const app = express()
-const port = 80
+const port = 3000
+const nodemailer = require("nodemailer");
+const { json } = require('stream/consumers');
 
 app.use(express.urlencoded({ extended: true }));
 
+
+const transporter = nodemailer.createTransport({
+  host: "smtp-mail.outlook.com",
+  auth: {
+    user: 'bartbikker@live.nl',
+    pass: 'Flitsdehond1!'
+  }
+});
+
 app.post('/offerte', (req, res) => {
-  const formDataOfferte = {
+  var formDataOfferte = {
     voornaam: req.body.fname,
     achternaam: req.body.lname,
     email: req.body.email,
-    streetname: req.body.streetname,
+    straatnaam: req.body.streetname,
     postcode: req.body.zipcode,
     stad: req.body.city,
     printtechniek: req.body.printtechniek,
@@ -17,7 +28,35 @@ app.post('/offerte', (req, res) => {
     opmerkingen: req.body.opmerkingen,
     bestand: req.body.bestand
   }
+
+  async function offerteEmail() {
+
+    const emailGegevens = await transporter.sendMail({
+      from: 'bartbikker@live.nl',
+      to: "bartbikker@live.nl", 
+      subject: "Offerte Aanvraag", 
+      text:"Nieuwe offerte aanvraag via de website gegevens:" + "\n\n" +
+           "Voornaam: " + formDataOfferte.voornaam + "\n" +
+           "Achternaam: " + formDataOfferte.achternaam + "\n" +
+           "Email: " + formDataOfferte.email + "\n" +
+           "Straatnaam: " + formDataOfferte.straatnaam + "\n" +
+           "Postcode: " + formDataOfferte.postcode + "\n" +
+           "Stad: " + formDataOfferte.stad + "\n" +
+           "Printtechniek: " + formDataOfferte.printtechniek + "\n" +
+           "Materiaal: " + formDataOfferte.materiaal + "\n" +
+           "Opmerkingen: " + req.body.opmerkingen + "\n" +
+           "Binnen 48 uur reageren",
+      attachments: [
+        {
+          filename:req.body.bestand ,
+          content: req.body.bestand
+        }
+      ]     
+    });
+    console.log("Message sent: %s", emailGegevens.messageId);
+  }
 console.log(formDataOfferte);
+offerteEmail();
 });
 
 
@@ -29,9 +68,33 @@ app.post('/contact', (req, res) => {
     postcode: req.body.zipcode,
     stad: req.body.city,
     opmerkingen: req.body.opmerkingen,
-
   }
   console.log(formDataContact);
+  async function contactEmail() {
+
+    const emailGegevens = await transporter.sendMail({
+      from: 'bartbikker@live.nl',
+      to: "bartbikker@live.nl", 
+      subject: "Contactformulier ingevuld", 
+      text:"Contactformulier ingevuld, reageren" + "\n\n" +
+           "Voornaam: " + formDataContact.naam + "\n" +
+           "Email: " + formDataContact.email + "\n" +
+           "Straatnaam: " + formDataContact.streetname + "\n" +
+           "Postcode: " + formDataContact.postcode + "\n" +
+           "Stad: " + formDataContact.stad + "\n" +
+           "Opmerkingen: " + formDataContact.opmerkingen + "\n" +
+           "Binnen 48 uur reageren",
+      attachments: [
+        {
+          filename:req.body.bestand ,
+          content: req.body.bestand
+        }
+      ]     
+    });
+    console.log("Message sent: %s", emailGegevens.messageId);
+  }
+console.log(formDataContact);
+contactEmail();
 });
 
 
