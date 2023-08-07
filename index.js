13779fa8a3820687b86249b69e8fcc6a2b412e42
BaBikker <104166110+BaBikker@users.noreply.github.com>
2023-08-07 15:14:05 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 const nodemailer = require("nodemailer");
-const { json } = require('stream/consumers');
 
 app.use(express.urlencoded({ extended: true }));
 
